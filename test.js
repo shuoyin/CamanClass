9cff15767ee1a331cb471a1d574cee271b24571f
yinshuo <yin1203014@gmail.com>
2015-10-10 17:31:07 +0800

    2015/10/10

--- test.js
+++ test.js
@@ -1,9 +1,6 @@
 function begin(){
 	var cxt = document.getElementById("kobe_canvas").getContext("2d");
-	Caman("#kobe_img",function(){
-		var a = this.getPixel(20,50);
-		console.log(a);
-	});
+	var pi = new this.PixelInfo(cxt);
 	var Animal;
 	Animal = (function(){
 		function Animal(name){
